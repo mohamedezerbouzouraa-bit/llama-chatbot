c07cf4f8a6372d23d20fba74cb82147be60ce42c
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-13 16:50:42 +0100

    Create interface.py

---
+++ app/interface.py
@@ -0,0 +1,17 @@
+import gradio as gr
+from .chat_model import chat
+
+# Load custom CSS
+with open("app/styles.css") as f:
+    css = f.read()
+
+def launch_ui():
+    with gr.Blocks(css=css) as demo:
+        chatbot = gr.Chatbot(height=400)
+        msg = gr.Textbox(placeholder="Type your message...", label="")
+        clear = gr.Button("Clear")
+
+        msg.submit(chat, [msg, chatbot], [msg, chatbot])
+        clear.click(lambda: None, None, chatbot, queue=False)
+
+    demo.launch()
